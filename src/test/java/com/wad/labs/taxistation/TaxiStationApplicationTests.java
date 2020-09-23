442e7d6ba77168abf5d6b6b5c5217fecafaa3dc3
Anton Treushchenko <anton.treushchenko@gmail.com>
2020-09-23 03:06:20 +0300

    Лабораторна робота №1

---
+++ src/test/java/com/wad/labs/taxistation/TaxiStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wad.labs.taxistation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
